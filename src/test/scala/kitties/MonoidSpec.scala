ebe1452731c2edb390e68a6922d9c4bf490cb9ed
Dave Gurnell <dave@cartographer.io>
2019-11-25 13:46:05 +0000

    Remove Part 2 code from Part 1 (again)

--- src/test/scala/kitties/MonoidSpec.scala
+++ src/test/scala/kitties/MonoidSpec.scala
@@ -7,12 +7,10 @@ class MonoidSpec extends FlatSpec with Matchers {
   "Monoid[A].empty" should "produce empty values" in {
     Monoid[Int].empty shouldBe 0
     Monoid[Double].empty shouldBe 0.0
-    Monoid[String].empty shouldBe ""
   }
 
   "x |+| y" should "add values" in {
     1 |+| 2 shouldBe 3
     1.1 |+| 2.2 shouldBe (3.3 +- 0.000001)
-    "hello" |+| "world" shouldBe "helloworld"
   }
 }
