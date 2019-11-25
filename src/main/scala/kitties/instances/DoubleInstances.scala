4ac106a15b73054343a59f6b12d56052a8660256
Dave Gurnell <dave@cartographer.io>
2019-11-25 14:04:09 +0000

    DELETE ALL THE THINGS

--- src/main/scala/kitties/instances/DoubleInstances.scala
+++ src/main/scala/kitties/instances/DoubleInstances.scala
@@ -6,7 +6,7 @@ trait DoubleInstances {
   implicit val doubleMonoid: Monoid[Double] =
     new Monoid[Double] {
       override def empty: Double =
-        0
+        0.0
 
       override def combine(x: Double, y: Double): Double =
         x + y
