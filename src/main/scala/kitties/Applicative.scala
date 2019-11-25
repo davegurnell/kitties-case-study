7c80fb0eeb75bd8f90e38d309af48c7775d63535
Dave Gurnell <dave@cartographer.io>
2019-11-25 11:05:43 +0000

    Delete unwanted files from part 1

--- src/main/scala/kitties/Applicative.scala
+++
@@ -1,6 +0,0 @@
-package kitties
-
-trait Applicative[F[_]] {
-  def pure[A](value: A): F[A]
-  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
-}
--- src/main/scala/kitties/Monad.scala
+++
@@ -1,5 +0,0 @@
-package kitties
-
-trait Monad[F[_]] {
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
-}
